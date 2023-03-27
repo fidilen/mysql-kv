7036e3a46361e78c199b7aa19fb9ebaa10f3aa98
fidilen <fidilen.dev@gmail.com>
2023-03-27 19:35:31 +0800

    added error hanling

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const kv = new KV(process.env.DATABASE_URL);
 
 // Executes every 15th minute of each hour
 schedule.scheduleJob('15 * * * *', async () => {
-    await kv.cleanup();
+    try {
+        await kv.cleanup();
+    } catch (e) {
+        console.error(e);
+    }
 });
