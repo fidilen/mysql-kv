7f9b2db8babe925353f2831114c5c4f1d3bdf5ee
fidilen <fidilen.dev@gmail.com>
2023-03-28 10:01:11 +0800

    added optional discord notification

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 const schedule = require('node-schedule');
 const { KV } = require('mysql-kv');
+const axios = require('axios');
 
 const kv = new KV(process.env.DATABASE_URL);
 
-// Executes every 15th minute of each hour
-schedule.scheduleJob('15 * * * *', async () => {
+// Executes every 15th minute of each hour, modify cron below
+schedule.scheduleJob('0 * * * *', async () => {
     try {
         await kv.cleanup();
+
+        await notifyDiscordWebhook(); // this is optional
     } catch (e) {
         console.error(e);
     }
 });
+
+async function notifyDiscordWebhook() {
+    axios.post(`${process.env.DISCORD_WEBHOOK_URL}`, {
+        username: "KV Cleaner",
+        avatar_url: "",
+        content: `Cleanup triggered: <t:${Math.floor(new Date().getTime() / 1000)}>`
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
